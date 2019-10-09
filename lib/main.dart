5bb0716a7869ad32b837c6c630031f93a8bb056e
Diego Ramos <da.ramos@uniandes.edu.co>
2019-10-09 10:36:48 -0500

    Move home screen class to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:t_app/home_screen.dart';
 
 void main() => runApp(TApp());
 
@@ -16,74 +13,3 @@ class TApp extends StatelessWidget {
   }
 }
 
-class MyHomeScreen extends StatefulWidget {
-  @override
-  _MyHomeScreenState createState() => _MyHomeScreenState();
-}
-
-class _MyHomeScreenState extends State<MyHomeScreen> {
-  GoogleMapController mapController;
-  String placeToFind;
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(controller) {
-    setState(() {
-      mapController = controller;
-    });
-  }
-
-  findPlace(){
-    Geolocator().placemarkFromAddress(placeToFind).then((result){
-      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        target: LatLng(result[0].position.latitude, result[0].position.longitude),
-        zoom: 10.0
-      )));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 11.0,
-            ),
-          ),
-          Positioned(
-            top: 30,
-            right: 15,
-            left: 15,
-            child: Container(
-              height: 50,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: findPlace,
-                    iconSize: 30.0,
-                  ),
-                ),
-                onChanged: (val){
-                  setState(() {
-                    placeToFind = val;
-                  });
-                }
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
