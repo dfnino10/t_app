ab5e2dc1b0a2d4a99088ce43b7f969215ede162a
Diego Ramos <da.ramos@uniandes.edu.co>
2019-11-05 14:20:32 -0500

    Schedule route snackbar and field checking

--- lib/ui/schedule_route.dart
+++ lib/ui/schedule_route.dart
@@ -5,7 +5,9 @@ import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:calendarro/calendarro.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:t_app/ui/custemWeekdayLabelsView.dart';
+import 'package:t_app/service/ConnectivityService.dart';
+import 'package:t_app/ui/connectivity_check.dart';
+import 'package:t_app/ui/customWeekdayLabelsView.dart';
 import 'package:t_app/ui/customDayTileBuilder.dart';
 import 'package:t_app/ui/custom_bottom_sheet.dart';
 
@@ -36,6 +38,10 @@ class _ScheduleRouteState extends State<ScheduleRoute> {
   String originName;
 
   String destName;
+
+  bool showButtons = true;
+
+  ConnectivityService conn = ConnectivityService();
 
   _ScheduleRouteState() {
     SharedPreferences.getInstance().then((SharedPreferences pref) {
@@ -81,7 +87,7 @@ class _ScheduleRouteState extends State<ScheduleRoute> {
     return firstDayOfCurrentMonth;
   }
 
-  Future<void> _selectTime(BuildContext context) async {
+  Future<void> selectTime(BuildContext context) async {
     final TimeOfDay picked = await showTimePicker(
       context: context,
       initialTime: TimeOfDay.now(),
@@ -94,6 +100,11 @@ class _ScheduleRouteState extends State<ScheduleRoute> {
   }
 
   onPressNext(BuildContext context) {
+
+    setState(() {
+      showButtons = false;
+    });
+
     for (int i = 0; i < selectedDates.length; i++) {
       CollectionReference docRef = Firestore.instance.collection('trips');
       docRef.add(<String, dynamic>{
@@ -108,14 +119,22 @@ class _ScheduleRouteState extends State<ScheduleRoute> {
       });
     }
 
-    print(Calendarro.of(context).selectedDates);
-    hideModalBottomSheetCustom(
-        context: context,
-        builder: (BuildContext bc) {
-          //emtpy on purpose
-        });
+    String snackbarText = conn.getConnectivityStatus() != ConnectivityResult.none ? "Viajes programados" : "No tienes conexión, tus viajes se programarán automáticamente cuando la recuperes";
 
-//    }
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text(snackbarText),
+      duration: Duration(seconds: 60),
+      action: SnackBarAction(
+        label: "Aceptar",
+        onPressed: () {
+          hideModalBottomSheetCustom(
+              context: context,
+              builder: (BuildContext bc) {
+                //emtpy on purpose
+              });
+        },
+      ),
+    ));
   }
 
   onPressBack() {
@@ -130,122 +149,146 @@ class _ScheduleRouteState extends State<ScheduleRoute> {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Column(
-          children: <Widget>[
-            Column(children: <Widget>[
-              Container(
-                  alignment: AlignmentDirectional.centerStart,
-                  padding: EdgeInsets.only(
-                    top: 5,
-                    bottom: 5,
-                    left: 5,
-                  ),
-                  child: Text(
-                    "Fechas de viaje",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        color: Theme.of(context).accentColor, fontSize: 16),
-                  )),
-              Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                        child: IconButton(
-                            icon: Icon(Icons.arrow_back),
-                            iconSize: 12,
-                            autofocus: false,
-                            onPressed: () {
-                              setState(() {
-                                currentMonth = (currentMonth - 1) % 12;
-                                calendarEndDate =
-                                    removeMonths(calendarEndDate, 1);
-                                calendarStartDate =
-                                    DateUtils.getFirstDayOfMonth(
-                                        calendarEndDate);
-                              });
-                            })),
-                    Container(
-                        padding: EdgeInsets.symmetric(horizontal: 5),
-                        child: Text(getCurrentMonthName(currentMonth) +
-                            " " +
-                            calendarStartDate.year.toString())),
-                    Container(
-                        child: IconButton(
-                      icon: Icon(Icons.arrow_forward),
-                      iconSize: 12,
-                      autofocus: false,
-                      onPressed: () {
-                        setState(() {
-                          currentMonth = (currentMonth + 1) % 12;
-                          calendarStartDate =
-                              DateUtils.addMonths(calendarStartDate, 1);
-                          calendarEndDate =
-                              DateUtils.getLastDayOfMonth(calendarStartDate);
-                        });
-                      },
-                    ))
-                  ]),
-              Calendarro(
-                weekdayLabelsRow: CustomWeekdayLabelsView(),
-                dayTileBuilder: CustomDayTileBuilder(),
-                startDate: calendarStartDate,
-                endDate: calendarEndDate,
-                displayMode: DisplayMode.MONTHS,
-                selectionMode: SelectionMode.MULTI,
-                onTap: (date) {
-                  setState(() {
-                    if (date.isAfter(DateUtils.toMidnight(DateTime.now())) &&
-                        !selectedDates.contains(date)) {
-                      selectedDates.add(date);
-                    } else {
-                      selectedDates.remove(date);
-                    }
-                  });
-                },
-              ),
-              Container(
-                  alignment: AlignmentDirectional.centerStart,
-                  padding: EdgeInsets.only(
-                    bottom: 5,
-                    left: 5,
-                  ),
-                  child: Text(
-                    "Hora de llegada:",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        color: Theme.of(context).accentColor, fontSize: 16),
-                  )),
-              Container(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: FlatButton(
-                      child: Text(selectedTime != null
-                          ? selectedTime.format(context)
-                          : TimeOfDay.now().format(context)),
-                      onPressed: () {
-                        _selectTime(context);
-                      }))
-            ]),
-            ButtonBar(
-              alignment: MainAxisAlignment.end,
+        body: Builder(
+          builder: (BuildContext context) {
+            return Column(
               children: <Widget>[
-                RaisedButton(
-                  child: Text("Cancelar"),
-                  onPressed: () {
-                    onPressBack();
-                  },
-                ),
-                RaisedButton(
-                  textColor: Colors.white,
-                  child: Text("Aceptar"),
-                  color: Theme.of(context).accentColor,
-                  onPressed: () {
-                    onPressNext(context);
-                  },
+                Column(children: <Widget>[
+                  Container(
+                      alignment: AlignmentDirectional.centerStart,
+                      padding: EdgeInsets.only(
+                        top: 5,
+                        bottom: 5,
+                        left: 5,
+                      ),
+                      child: Text(
+                        "Fechas de viaje",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: Theme.of(context).accentColor, fontSize: 16),
+                      )),
+                  Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                            child: IconButton(
+                                icon: Icon(Icons.arrow_back),
+                                iconSize: 12,
+                                autofocus: false,
+                                onPressed: () {
+                                  setState(() {
+                                    currentMonth = (currentMonth - 1) % 12;
+                                    calendarEndDate =
+                                        removeMonths(calendarEndDate, 1);
+                                    calendarStartDate =
+                                        DateUtils.getFirstDayOfMonth(
+                                            calendarEndDate);
+                                  });
+                                })),
+                        Container(
+                            padding: EdgeInsets.symmetric(horizontal: 5),
+                            child: Text(getCurrentMonthName(currentMonth) +
+                                " " +
+                                calendarStartDate.year.toString())),
+                        Container(
+                            child: IconButton(
+                          icon: Icon(Icons.arrow_forward),
+                          iconSize: 12,
+                          autofocus: false,
+                          onPressed: () {
+                            setState(() {
+                              currentMonth = (currentMonth + 1) % 12;
+                              calendarStartDate =
+                                  DateUtils.addMonths(calendarStartDate, 1);
+                              calendarEndDate = DateUtils.getLastDayOfMonth(
+                                  calendarStartDate);
+                            });
+                          },
+                        ))
+                      ]),
+                  Calendarro(
+                    weekdayLabelsRow: CustomWeekdayLabelsView(),
+                    dayTileBuilder: CustomDayTileBuilder(),
+                    startDate: calendarStartDate,
+                    endDate: calendarEndDate,
+                    displayMode: DisplayMode.MONTHS,
+                    selectionMode: SelectionMode.MULTI,
+                    onTap: (date) {
+                      setState(() {
+                        if (date.isAfter(
+                                DateUtils.toMidnight(DateTime.now())) &&
+                            !selectedDates.contains(date)) {
+                          selectedDates.add(date);
+                        } else {
+                          selectedDates.remove(date);
+                        }
+                      });
+                    },
+                  ),
+                  Container(
+                      alignment: AlignmentDirectional.centerStart,
+                      padding: EdgeInsets.only(
+                        bottom: 5,
+                        left: 5,
+                      ),
+                      child: Text(
+                        "Hora de llegada:",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: Theme.of(context).accentColor, fontSize: 16),
+                      )),
+                  Container(
+                      alignment: AlignmentDirectional.centerStart,
+                      child: FlatButton(
+                          child: Text(selectedTime != null
+                              ? selectedTime.format(context)
+                              : TimeOfDay.now().format(context)),
+                          onPressed: () {
+                            selectTime(context);
+                          }))
+                ]),
+                ButtonBar(
+                  alignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    if (showButtons)
+                      RaisedButton(
+                        child: Text("Cancelar"),
+                        onPressed: () {
+                          onPressBack();
+                        },
+                      ),
+                    if (showButtons)
+                      RaisedButton(
+                        textColor: Colors.white,
+                        child: Text("Aceptar"),
+                        color: Theme.of(context).accentColor,
+                        onPressed: () {
+                          if (selectedTime == null) {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text("Elige un tiempo de llegada")));
+                          } else if (originLat == null || originLon == null) {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content:
+                                    Text("Elige una ubicación de salida")));
+                          } else if (destLat == null || destLon == null) {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content:
+                                    Text("Elige una ubicación de destino")));
+                          } else if (selectedDates.length == 0) {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text(
+                                    "Elige por lo menos una fecha para los viajes")));
+                          } else {
+                            onPressNext(context);
+                          }
+                        },
+                      )
+                  ],
                 )
               ],
-            )
-          ],
+            );
+          },
         ));
   }
 }
